482f5e392e43ca683952d9d1e35bb882d5c3652b
Molly Lin <mollylin2014@gmail.com>
2024-05-10 16:37:08 +0800

    Complete Ch6, Ch7
    ch6: setting up Database
    ch7: fetch data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -70,7 +70,7 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
+    const numberOfInvoices = Number(data[0].rows[0].count ?? '0'); // TS's Nullish coalescing operator
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
